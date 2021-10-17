fcea736d290145aef38d76fe5ff11cdd4d870080
Ricardo Baloira <ricardobar96@gmail.com>
2021-10-17 14:45:17 +0100

    Actualizado proyecto

--- tarea_definicion_app/definicion_nueva_arquitectura/Booknown/Booknown/src/main/java/es/iespuerto/ricardo/booknown/AdminSQLiteOpenHelper.java
+++ tarea_definicion_app/definicion_nueva_arquitectura/Booknown/Booknown/src/main/java/es/iespuerto/ricardo/booknown/AdminSQLiteOpenHelper.java
@@ -1,4 +1,5 @@
 package es.iespuerto.ricardo.booknown;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -6,17 +7,41 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
+    SQLiteDatabase baseDeDatos = null;
+
     public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase baseDeDatos) {
-        baseDeDatos.execSQL("create table usuarios(idUsuario int primary key, nombre text, correo text, contrasenia text, avatar text, seguidores int, especial boolean)");
+        baseDeDatos.execSQL("create table usuarios(idUsuario int primary key autoincrement, nombre text, correo text, contrasenia text, avatar text, seguidores int, especial boolean)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        baseDeDatos.execSQL("DROP TABLE IF EXISTS usuarios");
+        onCreate(baseDeDatos);
+    }
+
+    public String addRecord(String nombre, String correo, String contrasenia, String avatar, int seguidores, boolean especial){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("nombre", nombre);
+        contentValues.put("correo", correo);
+        contentValues.put("contrasenia", contrasenia);
+        contentValues.put("avatar", avatar);
+        contentValues.put("seguidores", seguidores);
+        contentValues.put("especial", especial);
+
+        long resultado = database.insert("usuarios", null, contentValues);
+        if(resultado==-1){
+            return "No se pudo insertar datos en la tabla";
+        }
+        else{
+            return "Se han insertado los datos en la tabla";
+        }
     }
 }
--- tarea_definicion_app/definicion_nueva_arquitectura/Booknown/Booknown/src/main/java/es/iespuerto/ricardo/booknown/MainActivity.java
+++ tarea_definicion_app/definicion_nueva_arquitectura/Booknown/Booknown/src/main/java/es/iespuerto/ricardo/booknown/MainActivity.java
@@ -1,5 +1,6 @@
 package es.iespuerto.ricardo.booknown;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements ILoginView {
 
     EditText email,password;
     Button loginb;
+    Button botonCrearCuenta;
     ILoginController loginPresenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,20 @@ public class MainActivity extends AppCompatActivity implements ILoginView {
         email = (EditText) findViewById(R.id.email);
         password = (EditText)findViewById(R.id.password);
         loginb = (Button) findViewById(R.id.loginb);
+        botonCrearCuenta = (Button) findViewById(R.id.botonCrearCuenta);
         loginPresenter = new LoginController(this);
         loginb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginPresenter.OnLogin(email.getText().toString().trim(),password.getText().toString().trim());
+            }
+        });
+
+        botonCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PantallaCrearUsuario.class);
+                startActivity(intent);
             }
         });
     }
